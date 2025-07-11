d60db223d1ee782b24bcc64a850eafd77c476943
gregor <gjkcme@gmail.com>
2025-07-11 11:40:36 +0200

    Update news.ts
    s

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -2,17 +2,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fetchRSSFeeds from '@/lib/fetchRSSFeeds'
 
-let cachedArticles: any[] = []
+let cachedNews: Record<string, any[]> = {}
 let lastFetchTime: number = 0
 const CACHE_DURATION = 5 * 60 * 1000 // 5 minut
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const now = Date.now()
 
-  if (now - lastFetchTime > CACHE_DURATION || cachedArticles.length === 0) {
-    cachedArticles = await fetchRSSFeeds()
+  if (now - lastFetchTime > CACHE_DURATION || Object.keys(cachedNews).length === 0) {
+    cachedNews = await fetchRSSFeeds()
     lastFetchTime = now
   }
 
-  res.status(200).json(cachedArticles)
+  res.status(200).json(cachedNews)
 }
