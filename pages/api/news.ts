f9416fca25c24bb7eaa10248395f37f4dbaad1f3
gregor <gjkcme@gmail.com>
2025-07-11 09:52:48 +0200

    Update news.ts

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -1,23 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { fetchAllFeeds } from '@/lib/fetchRSSFeeds'
+import { fetchAllFeedsBySource } from '@/lib/fetchRSSFeeds'
 
-let cachedNews: any[] = []
+let cachedNewsBySource: Record<string, any[]> = {}
 let lastFetchTime: number = 0
 const CACHE_DURATION_MS = 45 * 60 * 1000 // 45 minut
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const now = Date.now()
 
-  if (cachedNews.length > 0 && now - lastFetchTime < CACHE_DURATION_MS) {
-    return res.status(200).json(cachedNews)
+  if (Object.keys(cachedNewsBySource).length > 0 && now - lastFetchTime < CACHE_DURATION_MS) {
+    return res.status(200).json(cachedNewsBySource)
   }
 
   try {
-    const news = await fetchAllFeeds()
-    cachedNews = news
+    const news = await fetchAllFeedsBySource()
+    cachedNewsBySource = news
     lastFetchTime = now
     res.status(200).json(news)
   } catch (error) {
+    console.error('Napaka pri pridobivanju novic:', error)
     res.status(500).json({ error: 'Napaka pri pridobivanju novic.' })
   }
 }
