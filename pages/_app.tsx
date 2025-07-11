7985c4a088eca79345034ff642ebef72dbd33c4a
gregor <gjkcme@gmail.com>
2025-07-11 13:08:48 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
+// /pages/_app.tsx
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { pageview } from '@/lib/ga'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
